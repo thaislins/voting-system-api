7598fca4cdeaab818d2cc07a8e6333b9c3365be2
Elton Viana <eltonvs@outlook.com>
2018-10-04 19:21:35 -0300

    Init Project

---
+++ src/main/java/br/ufrn/dimap/voting_system_api/App.java
@@ -0,0 +1,11 @@
+package br.ufrn.dimap.voting_system_api;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
